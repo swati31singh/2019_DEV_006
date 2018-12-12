fd6d1ca64138322e601599ffb9200bc15420da9f
swati123 <Swati.Singh@hibu.com>
2018-12-12 16:30:59 +0530

    Updated action on button click and added reset functionality.

--- TicTacToe/TicTacToe/View Controller/TicTacToeViewController.m
+++ TicTacToe/TicTacToe/View Controller/TicTacToeViewController.m
@@ -1,9 +1,14 @@
 
 #import "TicTacToeViewController.h"
 #import "TicTacToePresenter.h"
+#import "UIButton + TicTacToeAdditions.h"
+#import "Constants.h"
 
 
-@interface TicTacToeViewController ()
+NSInteger const kFirstPlayer = 1;
+NSInteger const kSecondPlayer = 2;
+
+@interface TicTacToeViewController () <TicTacToeGameProtocol>
 
 /* Outlets for game slot buttons */
 @property(weak, nonatomic) IBOutlet UIButton *slot0button;
@@ -40,17 +45,64 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self initialSetup];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)initialSetup {
+    self.presenter = [[TicTacToePresenter alloc] initWithDelegate:(id <TicTacToeGameProtocol>) self];
+    self.gameSlotButtons = [[NSMutableArray alloc] initWithObjects:self.slot0button, self.slot1button, self.slot2button, self.slot3button, self.slot4button, self.slot5button, self.slot6button, self.slot7button, self.slot8button, nil];
+    self.playAgainButton.backgroundColor = [UIColor blackColor];
+    self.currentPlayer = kFirstPlayer;
+    for (UIButton *button in self.gameSlotButtons) {
+        [button reset];
+    }
 }
 
 #pragma IBActions
 
 - (IBAction)updateSlot:(id)button {
-  // TO-DO - Action to be handled
+    if (self.currentPlayer == kFirstPlayer) {
+        [button updateForPlayer1];
+        self.currentPlayer = kSecondPlayer;
+    } else {
+        [button updateForPlayer2];
+        self.currentPlayer = kFirstPlayer;
+    }
+    
+    [self.presenter analyzeRowsColumnsAndDigonals:[self getValueFromBoard]];
 }
 
 - (IBAction)resetGame {
-  // TO-DO - Action to be handled
+    for (UIButton *button in self.gameSlotButtons) {
+        [button reset];
+    }
+    self.winningTextLabel.text = EMPTY_TITLE_STRING;
+    self.currentPlayer = kFirstPlayer;
+    self.playAgainButton.hidden = YES;
+    self.resetButton.hidden = NO;
+}
+
+#pragma Private methods
+
+- (NSArray *)getValueFromBoard {
+    NSMutableArray *valueArray = [[NSMutableArray alloc] init];
+    for (UIButton *button in self.gameSlotButtons) {
+        if (button.currentTitle) {
+            [valueArray addObject:button.currentTitle];
+        }
+    }
+    return valueArray;
+}
+
+#pragma  Protocol methods
+
+- (void)finishGameWithWinner {
+    // To-Do: Handle the game over condition here.
+}
+
+- (void)proceedGame {
+   // To-Do: Handle the condition to proceed the game here.
 }
 
 
